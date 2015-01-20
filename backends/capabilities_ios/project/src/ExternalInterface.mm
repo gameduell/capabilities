6f32e9e3c734e5ca01344ff0ba67fbedd5aacb91
kgar <khaled.garbaya@gamduell.de>
2015-01-20 16:39:44 +0100

    code cleanup

--- backends/capabilities_ios/project/src/ExternalInterface.mm
+++ backends/capabilities_ios/project/src/ExternalInterface.mm
@@ -10,15 +10,18 @@ static value ioscapabilities_getDeviceOrientation()
     UIDeviceOrientation dOrientation = [UIDevice currentDevice].orientation;
     UIInterfaceOrientation iOrientation = [UIApplication sharedApplication].statusBarOrientation;
     bool landscape;
-	if (dOrientation == UIDeviceOrientationUnknown || dOrientation == UIDeviceOrientationFaceUp || dOrientation == UIDeviceOrientationFaceDown) {
+	if (dOrientation == UIDeviceOrientationUnknown || dOrientation == UIDeviceOrientationFaceUp || dOrientation == UIDeviceOrientationFaceDown)
+	{
 		// If the device is laying down, use the UIInterfaceOrientation based on the status bar.
 		landscape = UIInterfaceOrientationIsLandscape(iOrientation);
-	} else {
+	} else
+	{
 		// If the device is not laying down, use UIDeviceOrientation.
 		landscape = UIDeviceOrientationIsLandscape(dOrientation);
 	}
 
-	if (landscape) {
+	if (landscape)
+	{
 		return alloc_int(0);
 	}
 
