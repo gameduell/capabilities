9a0eecb383a6db670b7d339a19d3a29a49b9518f
Rui Campos <rui.campos@gameduell.de>
2015-04-15 15:01:05 +0200

    ci script update

--- ci.sh
+++ ci.sh
@@ -7,7 +7,7 @@ rm -rf Export
 
 haxelib run duell update -verbose -yestoall
 
-haxelib run duell build android -test -verbose -D jenkins -yestoall
+haxelib run duell build android -test -verbose -D jenkins -yestoall -emulator -x86
 
 haxelib run duell build html5 -test -verbose -D jenkins -yestoall
 
