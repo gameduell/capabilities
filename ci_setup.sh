abbff4b69ab332c887bec1f7873530f3fb9d4cad
Christian Lührs <christian.luehrs@gameduell.de>
2016-01-19 18:13:24 +0100

    ci adjustments

--- ci_setup.sh
+++ ci_setup.sh
@@ -4,8 +4,9 @@ set -e
 trap onexit 1 2 3 15 ERR
 
 function onexit() {
-    echo "Problem occured during setup, exit code: $?"
-    exit $exit_status
+    error=$?
+    echo "Problem occured during setup, exit code: ${error}"
+    exit $error
 }
 
 cd tests
@@ -15,19 +16,23 @@ rm -rf Export
 printf '\n'
 printf ' ---------- UPDATE DUELL DEPENDENCIES ----------'
 printf '\n'
+printf '\n'
 haxelib run duell_duell update -verbose -yestoall
 
 printf '\n'
 printf ' ---------- BUILD ANDROID BINARY ----------'
+printf '\n'
 printf '\n'
 haxelib run duell_duell build android -norun -verbose -yestoall -x86 -D jenkins
 
 printf '\n'
 printf ' ---------- BUILD IOS BINARY ----------'
 printf '\n'
+printf '\n'
 haxelib run duell_duell build ios -norun -simulator -verbose -yestoall -D jenkins
 
 printf '\n'
 printf ' ---------- BUILD HTML TARGET ----------'
+printf '\n'
 printf '\n'
 haxelib run duell_duell build html5 -norun -verbose -D jenkins -yestoall
